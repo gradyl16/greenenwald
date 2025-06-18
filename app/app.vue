5a9f43e671e8d47df535407c34ff1064250e7be9
Dylen Greenenwald <gradyl16@protonmail.com>
2025-06-17 22:57:38 -0500

    chore: seo meta tags

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+
+useSeoMeta({
+  title: 'Dylen Greenenwald',
+  ogTitle: 'Dylen Greenenwald',
+  description: 'Explorer. Security researcher. Juggler. Come find out what else I am at my personal website.',
+  ogDescription: 'Explorer. Security researcher. Juggler. Come find out what else I am at my personal website.',
+  ogImage: 'https://greenenwald.com/portrait.jpg',
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
