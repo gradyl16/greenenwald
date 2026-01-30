6faf9bf490c8995ce3daff50c0f6746392219863
Dylen Greenenwald <dgree21@illinois.edu>
2026-01-29 23:04:55 -0600

    chore: delete temp code from recovery

--- app/components/Publication.vue
+++ app/components/Publication.vue
@@ -1,38 +1,3 @@
-<!-- <template> -->
-<!--   <article -->
-<!--     class="group rounded-xl border border-gray/40 bg-dark-gray/40 p-5 sm:p-6 transition hover:border-white/60 hover:bg-test/60" -->
-<!--   > -->
-<!--     <h4 class="text-cyan transition"> -->
-<!--       {{ pub.title }} -->
-<!--     </h4> -->
-<!---->
-<!--     <p class="text-purple">{{ pub.authors.join(", ") }}.</p> -->
-<!---->
-<!--     <p class="text-purple"> -->
-<!--       <span v-if="!pub.published" class="italic opacity-80">(To appear)</span> -->
-<!--       <span> In the proceedings of </span> -->
-<!--       <a -->
-<!--         class="text-pink underline hover:text-pink/80" -->
-<!--         :href="pub.venue.url" -->
-<!--         target="_blank" -->
-<!--         rel="noopener" -->
-<!--       > -->
-<!--         {{ pub.venue.name }} -->
-<!--       </a> -->
-<!--     </p> -->
-<!---->
-<!--     <p class="mt-1 text-sm text-purple/80"> -->
-<!--       {{ pub.venue.date }}, {{ pub.venue.loc }} -->
-<!--     </p> -->
-<!--   </article> -->
-<!-- </template> -->
-<!---->
-<!-- <script setup lang="ts"> -->
-<!-- // import type { Publication } from "../../types/publication"; -->
-<!-- const props = defineProps<{pub: Publication}>(); -->
-<!---->
-<!-- </script> -->
-
 <template>
   <article
     class="group rounded-xl border border-gray/40 bg-dark-gray/40 p-5 sm:p-6 transition hover:border-white/60 hover:bg-test/60"
